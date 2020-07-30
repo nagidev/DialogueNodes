21560d76ac5cbf48a70da11c8bdbbc6012945824
bhgt <bhagat.nagi@gmail.com>
2020-07-30 07:06:41 +0530

    Switched from bottom panel to main screen

--- addons/dialogue_nodes/plugin.gd
+++ addons/dialogue_nodes/plugin.gd
@@ -6,11 +6,34 @@ var scene
 
 func _enter_tree():
 	scene = preload("res://addons/dialogue_nodes/NodeEditor.tscn").instance()
-	add_control_to_bottom_panel(scene, 'Dialogue Nodes')
+	#add_control_to_bottom_panel(scene, 'Dialogue Nodes')
+	
+	get_editor_interface().get_editor_viewport().add_child(scene)
+	make_visible(false)
 	print('Plugin Enabled')
 
 
 func _exit_tree():
-	remove_control_from_bottom_panel(scene)
+	#remove_control_from_bottom_panel(scene)
+	# Remove from main viewport
+	if scene:
+		scene.queue_free()
 	scene.free()
 	print('Plugin Disabled')
+
+
+func has_main_screen():
+	return true
+
+
+func make_visible(visible):
+	if scene:
+		scene.visible = visible
+
+
+func get_plugin_name():
+	return "Dialogue Nodes"
+
+
+func get_plugin_icon():
+	return get_editor_interface().get_base_control().get_icon("Script", "EditorIcons")
