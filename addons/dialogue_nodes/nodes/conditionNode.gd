aa6c2b58fd340d960f1fd864bd6f00288b318418
bhgt <bhagat.nagi@gmail.com>
2022-02-13 15:20:54 +0530

    Added Condition functionality

---
+++ addons/dialogue_nodes/nodes/conditionNode.gd
@@ -0,0 +1,53 @@
+tool
+extends GraphNode
+
+enum {EQUAL, NEQUAL, GREATER, LESS, GEQUAL, LEQUAL}
+
+signal modified
+
+onready var value1 = $HBoxContainer/Value1
+onready var operator = $HBoxContainer/Operator
+onready var value2 = $HBoxContainer/Value2
+
+onready var trueLabel = $TrueLabel
+onready var falseLabel = $FalseLabel
+
+
+func _to_dict(graph):
+	var dict = {}
+	dict['value1'] = value1.text
+	dict['operator'] = operator.selected
+	dict['value2'] = value2.text
+	
+	dict['true'] = 'END'
+	dict['false'] = 'END'
+	
+	for connection in graph.get_connection_list():
+		if connection['from'] == name:
+			if connection['from_port'] == 0:
+				dict['true'] = connection['to']
+			elif connection['from_port'] == 1:
+				dict['false'] = connection['to']
+	
+	return dict
+
+
+func _from_dict(_graph, dict):
+	value1.text = dict['value1']
+	operator.selected = dict['operator']
+	value2.text = dict['value2']
+	
+	
+	return [dict['true'], dict['false']]
+
+
+func set_type(_new_type):
+	_on_modified()
+
+
+func set_value(_new_val):
+	_on_modified()
+
+
+func _on_modified():
+	emit_signal("modified")
--- addons/dialogue_nodes/nodes/setNode.gd
+++ addons/dialogue_nodes/nodes/setNode.gd
@@ -5,9 +5,9 @@ enum {STRING, INT, FLOAT, BOOL}
 
 signal modified
 
-onready var variable = $Variable
+onready var variable = $HBoxContainer/Variable
 onready var type = $HBoxContainer/Type
-onready var value = $Value
+onready var value = $HBoxContainer/Value
 
 
 func _to_dict(graph):
@@ -19,7 +19,7 @@ func _to_dict(graph):
 	var next_nodes = graph.get_next(name)
 	
 	if len(next_nodes) > 0:
-		dict['link'] = graph.get_next(name)[0]
+		dict['link'] = next_nodes[0]
 	else:
 		dict['link'] = 'END'
 	
