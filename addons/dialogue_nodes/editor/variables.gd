be09ea5a7d314d44bad0ee19c827ac7d74d74734
nagidev <bhagat.nagi@gmail.com>
2023-11-03 01:09:36 +0530

    Updated variables section

--- addons/dialogue_nodes/editor/variables.gd
+++ addons/dialogue_nodes/editor/variables.gd
@@ -5,51 +5,17 @@ extends Control
 signal modified
 
 
-var delete_icon = preload("res://addons/dialogue_nodes/icons/Remove.svg")
-var types = [TYPE_STRING, TYPE_INT, TYPE_FLOAT, TYPE_BOOL]
+var variable_item_scene = preload('res://addons/dialogue_nodes/editor/VariableItem.tscn')
 
-
-func add_variable(new_name='', new_type=0, new_value = ''):
-	# Variable container
-	var new_variable = HBoxContainer.new()
-	new_variable.name = 'Variable0'
+func add_variable(new_name= '', new_type= TYPE_STRING, new_value= ''):
+	var new_variable = variable_item_scene.instantiate()
 	add_child(new_variable, true)
 	
-	# Name
-	var var_name = LineEdit.new()
-	var_name.name = 'Name'
-	var_name.text = new_name
-	var_name.placeholder_text = 'Variable Name'
-	var_name.size_flags_horizontal = SIZE_EXPAND_FILL
-	new_variable.add_child(var_name)
-	var _res = var_name.text_changed.connect(_on_modified)
-	
-	# Type
-	var type = OptionButton.new()
-	type.name = 'Type'
-	type.add_item('String', TYPE_STRING)
-	type.add_item('Int', TYPE_INT)
-	type.add_item('Float', TYPE_FLOAT)
-	type.add_item('Bool', TYPE_BOOL)
-	new_variable.add_child(type, true)
-	type.selected = types.find(new_type)
-	_res = type.item_selected.connect(_on_modified)
-	
-	# Value
-	var var_val = LineEdit.new()
-	var_val.name = 'Value'
-	var_val.text = new_value
-	var_val.placeholder_text = 'Value'
-	var_val.size_flags_horizontal = SIZE_EXPAND_FILL
-	new_variable.add_child(var_val)
-	_res = var_val.text_changed.connect(_on_modified)
-	
-	# Delete button
-	var delete = Button.new()
-	delete.name = 'DeleteButton'
-	new_variable.add_child(delete)
-	delete.icon = delete_icon
-	delete.connect("pressed", Callable(self, "remove_variable").bind(new_variable))
+	new_variable.var_name.text = new_name
+	new_variable.type.select(new_variable.types.find(new_type))
+	new_variable._on_type_changed(new_variable.types.find(new_type))
+	new_variable.set_value(new_value)
+	new_variable.modified.connect(_on_modified)
 	
 	_on_modified()
 	
@@ -69,7 +35,7 @@ func remove_all_variables():
 
 func get_variable(var_name):
 	for child in get_children():
-		if child is HBoxContainer and child.get_node('Name').text == var_name:
+		if child is HBoxContainer and child.var_name.text == var_name:
 			return child
 	
 	printerr('Variable not found : ', var_name)
@@ -77,12 +43,11 @@ func get_variable(var_name):
 
 
 func get_value(var_name):
-	return get_variable(var_name).get_node('Value').text
+	return get_variable(var_name).get_value()
 
 
 func set_value(var_name, value):
-	var variable = get_variable(var_name)
-	variable.get_node('Value').text = str(value)
+	get_variable(var_name).set_value(value)
 
 
 func to_dict():
@@ -90,9 +55,9 @@ func to_dict():
 	
 	for child in get_children():
 		if child is HBoxContainer:
-			var var_name = child.get_node('Name').text
-			var type = child.get_node('Type').get_item_id(child.get_node('Type').selected)
-			var var_val = child.get_node('Value').text
+			var var_name = child.var_name.text
+			var type = child.type.get_item_id(child.type.selected)
+			var var_val = child.get_value()
 			if var_name != '':
 				dict[var_name] = {'type': type, 'value': var_val}
 	
@@ -108,5 +73,5 @@ func from_dict(dict):
 		add_variable(var_name, int(dict[var_name]['type']), dict[var_name]['value'])
 
 
-func _on_modified(_a=0, _b=0):
+func _on_modified(_a= 0, _b= 0):
 	modified.emit()
