b586daafcce7e375e1cfe1dedaf30c8b749ba11c
nagidev <bhagat.nagi@gmail.com>
2023-11-25 18:44:34 +0530

    Fixed icon theming and scaling issues

--- addons/dialogue_nodes/editor/editor.gd
+++ addons/dialogue_nodes/editor/editor.gd
@@ -70,9 +70,9 @@ func _run_tree(start_node):
 	data = start_node.tree_to_data(graph, data)
 	data.variables = variables.to_dict()
 	data.characters = characters.filePath.text
-	dialogue.data = data
+	dialogue.dialogue_data = data
 	
-	if dialogue.data.starts.has(start_node.ID):
+	if dialogue.dialogue_data.starts.has(start_node.ID):
 		dialogueBG.show()
 		dialogue.options.get_child(0).grab_focus()
 		dialogue.start(start_node.ID)
