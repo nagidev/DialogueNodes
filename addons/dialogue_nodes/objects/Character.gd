b31a660a099cee08ccd3cb6b60f524ef0b2c162d
Deccir <92213294+Deccir@users.noreply.github.com>
2024-06-23 12:42:26 +0200

    Fixed character names not being translated after switching locale

--- addons/dialogue_nodes/objects/Character.gd
+++ addons/dialogue_nodes/objects/Character.gd
@@ -3,11 +3,10 @@
 class_name Character
 extends Resource
 
-@export var name : String = '' :
-    set(value):
-        name = value
-        translated_name = tr(value)
+@export var name : String = ''
 @export var image : Texture2D
 @export var color : Color = Color.WHITE
 
-var translated_name : String = ''
+var translated_name : String :
+    get:
+        return tr(name)
